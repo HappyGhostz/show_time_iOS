197d32bb659cf3115a6702175f339c0f06237c94
Chenping Zhao <chenping.zhao@grabtaxi.com>
2020-10-15 17:17:50 +0800

    Initial Commit

---
+++ Show Time/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Show Time
+//
+//  Created by Chenping Zhao on 2020/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
