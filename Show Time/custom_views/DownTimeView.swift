cd81570bb7a9d40443f23c824a0d2ac387e2c0c7
Chenping Zhao <chenping.zhao@grabtaxi.com>
2020-10-16 15:52:51 +0800

    update view

--- Show Time/custom_views/DownTimeView.swift
+++ Show Time/custom_views/DownTimeView.swift
@@ -19,7 +19,7 @@ public class DownTimeView: UIView{
         super.init(coder: coder)
     }
     
-//    override func layoutSubviews() {
-//
-//    }
+    public override func layoutSubviews() {
+
+    }
 }
