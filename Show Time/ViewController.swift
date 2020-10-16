1d772692d914b25b00ae9d976d838ca423bb3241
Chenping Zhao <chenping.zhao@grabtaxi.com>
2020-10-16 15:09:51 +0800

    Init Show Time for iOS, and set lunch screen and play gif.

--- Show Time/ViewController.swift
+++ Show Time/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let jeremyGif  = UIImage.gifImageWithName(name: "splash")
+        let imageView = UIImageView(image: jeremyGif)
+        imageView.frame = CGRect(x:0.0,y: 0.0, width: self.view.frame.size.width,height: self.view.frame.size.height)
+        imageView.animationRepeatCount = 0
+        imageView.startAnimating()
+        view.addSubview(imageView)
     }
-
 
 }
 
---
+++ Show Time/custom_views/DownTimeView.swift
@@ -0,0 +1,25 @@
+//
+//  DownTimeView.swift
+//  Show Time
+//
+//  Created by Chenping Zhao on 2020/10/16.
+//
+
+import UIKit
+public class DownTimeView: UIView{
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    convenience init(){
+        self.init(frame: CGRect.zero)
+    }
+    
+    override public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+//    override func layoutSubviews() {
+//
+//    }
+}
